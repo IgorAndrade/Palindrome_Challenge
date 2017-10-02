21c5daf6c9e774defa1703d3c9f0b926ece6532b
Igor Andrade <andrade.igor@gmail.com>
2017-10-02 20:23:28 -0300

    building test

--- test/router.test.js
+++ test/router.test.js
@@ -4,29 +4,19 @@ let app = require("../config/express"),
 
 let expect = chai.expect;
 
-describe('test the palindrome function', function () {
+xdescribe('test the palindrome function', function () {
     describe('palindrome words', function () {
 
         it('correct palindrome: ABA', function (done) {
-            request(app).get('/isPalindrome?test=ABA').end(function (err, res) {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
-        })
+            request(app).get('/disPalindrome?test=ANA').expect(200, done)
+        });
+
         it('correct palindrome: alula', function (done) {
-            request(app).get('/isPalindrome?test=alula').end(function (err, res) {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
+            request(app).get('/isPalindrome?test=alula').expect(200, done)
         })
         it('incorrect palindrome: MACACO', function (done) {
-            request(app).get('/isPalindrome?test=MACACO').end(function (err, res) {
-                expect(res.statusCode).to.equal(200);
-                done();
-            });
-        })
-
+            request(app).get('/isPalindrome?test=MACACO').expect(400,done)
+        });
     })
 
-    
-})
+});
